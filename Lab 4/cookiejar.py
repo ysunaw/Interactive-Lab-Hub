f1ef7464a9db3de3325237ca6f8f017aef04fa5c
ysunaw <ysunaw@gmail.com>
2021-04-06 21:40:15 -0400

    finished lab4

--- Lab 4/cookiejar.py
+++ Lab 4/cookiejar.py
@@ -4,6 +4,7 @@ import busio
 from time import strftime, sleep
 import adafruit_mpr121
 import digitalio
+from PIL import Image, ImageDraw, ImageFont
 
 from adafruit_rgb_display.rgb import color565
 import adafruit_rgb_display.st7789 as st7789
@@ -23,7 +24,10 @@ dc_pin = digitalio.DigitalInOut(board.D25)
 reset_pin = None
 BAUDRATE = 64000000  # the rate  the screen talks to the pi
 # Create the ST7789 display:
-display = st7789.ST7789(
+buttonPin = 18
+font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 18)
+
+disp = st7789.ST7789(
     board.SPI(),
     cs=cs_pin,
     dc=dc_pin,
@@ -33,26 +37,35 @@ display = st7789.ST7789(
     height=240,
     x_offset=53,
     y_offset=40,
+    rotation = 90
 )
 if disp.rotation % 180 == 90:
     height = disp.width  # we swap height/width to rotate it to landscape!
     width = disp.height
 else:
-    width = disp.width  # we swap height/width to rotate it to landscape!
-    height = disp.height
+    width = disp.height  # we swap height/width to rotate it to landscape!
+    height = disp.width
 
 image = Image.new("RGB", (width, height))
 draw = ImageDraw.Draw(image)
 draw.rectangle((0, 0, width, height), outline=0, fill=(0, 0, 0))
-x = 35
+x = 20
 y = height //2 -30
-draw.text((x,y), "Don't touch my cookie", font=font, fill=(255, 255, 255))
+draw.text((x,y), "Don't touch my cookie! ", font=font, fill=(255, 0, 0))
 disp.image(image)
 
 
 while True:
     for i in range(12):
         if mpr121[i].value:
-            print(f"Banana {i} touched at ", strftime("%m/%d/%Y %H:%M:%S"))
-            break
-    time.sleep(0.25)  # Small delay to keep from spamming output messages.
+            print("stolen!")
+            text = "Someone stole the cookie\n from the cookie jar\n at" + strftime("%m/%d/%Y %H:%M:%S")
+            image = Image.new("RGB", (width, height))
+            draw = ImageDraw.Draw(image)
+            draw.rectangle((0, 0, width, height), outline=0, fill=(0, 0, 0))
+            x = 10
+            y = height //2 -30
+            draw.text((x,y), text, font=font, fill=(255, 0, 0))
+            disp.image(image)
+
+    time.sleep(0.1)  # Small delay to keep from spamming output messages.
